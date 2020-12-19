9cd7802929741463758c4c47c79a58e48f321473
gabriel.bucalon.dex <bucalongabriell@gmail.com>
2020-12-19 16:08:03 -0300

    first commit

---
+++ src/main/java/br/com/empenhados/praOndeVou/PraOndeVouApplication.java
@@ -0,0 +1,13 @@
+package br.com.empenhados.praOndeVou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraOndeVouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraOndeVouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empenhados/praOndeVou/PraOndeVouApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empenhados.praOndeVou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraOndeVouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

705c996ec1700dd2c511ffa7938b14fe8df58201
gabriel.bucalon.dex <bucalongabriell@gmail.com>
2020-12-29 11:27:36 -0300

    feature: structuring the model

---
+++ src/main/java/br/com/empenhados/praOndeVou/models/Pontos.java
@@ -0,0 +1,15 @@
+package br.com.empenhados.praOndeVou.models;
+
+public class Pontos {
+    private long id;
+    private String cep;
+    private int numero;
+    private String bairro;
+    private String estado;
+    private String nomeLocalidade;
+    private double latitudade;
+    private double lontitude;
+    private long usuario_id;
+
+
+}

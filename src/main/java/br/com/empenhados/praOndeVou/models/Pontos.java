a77d90930d1f669b4d82e5c77e45d1be70d30643
gabriel.bucalon.dex <bucalongabriell@gmail.com>
2020-12-29 11:29:40 -0300

    feature: create getters ands setters. Create toString to viewed datas objet

--- src/main/java/br/com/empenhados/praOndeVou/models/Pontos.java
+++ src/main/java/br/com/empenhados/praOndeVou/models/Pontos.java
@@ -11,5 +11,92 @@ public class Pontos {
     private double lontitude;
     private long usuario_id;
 
+    public Pontos(){ }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNomeLocalidade() {
+        return nomeLocalidade;
+    }
+
+    public void setNomeLocalidade(String nomeLocalidade) {
+        this.nomeLocalidade = nomeLocalidade;
+    }
+
+    public double getLatitudade() {
+        return latitudade;
+    }
+
+    public void setLatitudade(double latitudade) {
+        this.latitudade = latitudade;
+    }
+
+    public double getLontitude() {
+        return lontitude;
+    }
+
+    public void setLontitude(double lontitude) {
+        this.lontitude = lontitude;
+    }
+
+    public long getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(long usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Pontos{" +
+                "id=" + id +
+                ", cep='" + cep + '\'' +
+                ", numero=" + numero +
+                ", bairro='" + bairro + '\'' +
+                ", estado='" + estado + '\'' +
+                ", nomeLocalidade='" + nomeLocalidade + '\'' +
+                ", latitudade=" + latitudade +
+                ", lontitude=" + lontitude +
+                ", usuario_id=" + usuario_id +
+                '}';
+    }
 }

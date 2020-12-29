4c1f7242eca137d3ae442ac032e648c12037a474
gabriel.bucalon.dex <bucalongabriell@gmail.com>
2020-12-29 11:50:09 -0300

    feature: set method to save ponto

--- src/main/java/br/com/empenhados/praOndeVou/controllers/PontosController.java
+++ src/main/java/br/com/empenhados/praOndeVou/controllers/PontosController.java
@@ -1,13 +1,31 @@
 package br.com.empenhados.praOndeVou.controllers;
 
+import br.com.empenhados.praOndeVou.models.Ponto;
+import br.com.empenhados.praOndeVou.repositories.PontoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PontosController {
+    @Autowired
+    private PontoRepository pontoRepository;
 
+    // Método responsavel para redirecionar
+    // usuário para pagina de cadastrar um novo ponto.
     @GetMapping("/pontos/new")
     public String neww() {
         return "pontos/new";
     }
+
+    // Método responsavel para criar um novo ponto
+    @GetMapping("/pontos")
+    public String create(Ponto ponto){
+
+//        pontoRepository.save(ponto);
+
+        return "redirect:/";
+    }
+
+
 }
